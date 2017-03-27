df8b7a881ff1b018e1495e12c3a207b45d4c886f
Lucas Ferraz Paolillo <lucas_cfp26@hotmail.com>
2017-03-27 12:56:49 -0300

    add concurrency teste

--- rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
+++ rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
@@ -7,9 +7,11 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Maybe;
+import io.reactivex.MaybeSource;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.Single;
+import io.reactivex.SingleSource;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.BiPredicate;
 import io.reactivex.functions.Function;
@@ -61,7 +63,8 @@ class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillionWithTimeoutFallbackToEmpty(final FutureTask<List<Country>> countriesFromNetwork) {
-        return Observable.fromFuture(countriesFromNetwork, 1, TimeUnit.HOURS)
+        return Observable.fromFuture(countriesFromNetwork)
+                .timeout(1 ,TimeUnit.HOURS)
                 .flatMapIterable(country -> country)
                 .filter(country -> country.getPopulation() > 1000000);
 
@@ -70,7 +73,10 @@ class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
-        return null;
+        return Observable.fromIterable(countries)
+                .filter(country -> country.getName().equals(countryName))
+                .flatMap(country -> Observable.just(country.getCurrency()))
+                .defaultIfEmpty("USD");
     }
 
     @Override
@@ -100,6 +106,9 @@ class CountriesServiceSolved implements CountriesService {
     @Override
     public Single<Boolean> areEmittingSameSequences(Observable<Country> countryObservable1,
                                                     Observable<Country> countryObservable2) {
-        return Observable.sequenceEqual(countryObservable1, countryObservable2, (country, country2) -> country.getName().equals(country2.getName()));
+        return Observable
+                .sequenceEqual(countryObservable1, countryObservable2,
+                        (country, country2) -> country.getName().equals(country2.getName())
+                );
     }
 }
